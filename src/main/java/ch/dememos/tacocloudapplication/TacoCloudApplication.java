5f9372f4b5d6288d52447bcef55ac194117f20ba
Irina Kochenova <dememos@mail.ru>
2024-07-04 16:55:42 +0200

    home page

---
+++ src/main/java/ch/dememos/tacocloudapplication/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package ch.dememos.tacocloudapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
