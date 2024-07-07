c774fb0a9ea4aafb1a7028c90bc38788d34e3526
Irina Kochenova <dememos@mail.ru>
2024-07-07 14:11:55 +0200

    deleted Home controller, turned off cache, added validation span in html

--- src/test/java/ch/dememos/tacocloudapplication/HomeControllerTest.java
+++ src/test/java/ch/dememos/tacocloudapplication/HomeControllerTest.java
@@ -1,6 +1,5 @@
 package ch.dememos.tacocloudapplication;
 
-import ch.dememos.tacocloudapplication.controllers.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,7 +9,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest(WebConfig.class)
 public class HomeControllerTest {
 
     @Autowired
