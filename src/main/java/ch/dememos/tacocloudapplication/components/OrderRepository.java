63fb703af533b44cbe4fc2483b66328ba8bd83f8
Irina Kochenova <dememos@mail.ru>
2024-07-10 16:22:59 +0200

    add extra method

--- src/main/java/ch/dememos/tacocloudapplication/components/OrderRepository.java
+++ src/main/java/ch/dememos/tacocloudapplication/components/OrderRepository.java
@@ -3,5 +3,9 @@ package ch.dememos.tacocloudapplication.components;
 import ch.dememos.tacocloudapplication.model.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
 }
