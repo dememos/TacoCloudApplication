4f26b6e497295de5f6109dfa45084c70876afae0
Irina Kochenova <dememos@mail.ru>
2024-07-12 16:20:52 +0200

    fix problem with mongo db

--- src/main/java/ch/dememos/tacocloudapplication/model/Ingredient.java
+++ src/main/java/ch/dememos/tacocloudapplication/model/Ingredient.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-@Document(collation = "ingredients")
+@Document
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @Data
