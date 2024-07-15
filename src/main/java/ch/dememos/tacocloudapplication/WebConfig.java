48bf8d3a9222db8d40524c276843bf8f518892dd
Irina Kochenova <dememos@mail.ru>
2024-07-15 19:50:27 +0200

    removed no needed stuff

--- src/main/java/ch/dememos/tacocloudapplication/WebConfig.java
+++ src/main/java/ch/dememos/tacocloudapplication/WebConfig.java
@@ -1,16 +1,11 @@
 package ch.dememos.tacocloudapplication;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
